0a0ed2cb0637109272819e793bea708561ccd7c7
ej <evanrjohnso@gmail.com>
2017-10-13 17:19:57 -0700

    basic styling for main activity

---
+++ app/README.md
@@ -0,0 +1,54 @@
+# QuickQuiz
+
+## Description
+
+_A gamefied learning platform for all things knowledge!_
+_Make studying more engaging. Learn throughout the day with pings from the app!_
+
+## Setup/Installation Requirements
+
+* Android Studio
+* Android Emulator
+
+```console
+  git clone https://github.com/evanjohnso/DocDocGo.git
+```
+
+```console
+  cd DocDocGo
+```
+
+```console
+  npm install
+```
+
+```console
+  gulp serve
+```
+
+
+
+## Development Specifications
+| Behavior      | Example Input         | Example Output        |
+| ------------- | ------------- | ------------- |
+| Enter Location && speciality | Seattle, Dentist  |  15 doctors in your area!  |
+
+## Output Examples
+![main screen](img/display1.png)
+***
+![After clicking on doctor](img/display2.png)
+***
+
+
+
+## Technologies Used
+
+* _Android Studio_
+
+## Known Bugs
+* 
+
+
+### License
+
+MIT Copyright &copy; 2017 Evan Johnson
--- app/src/main/java/com/evanrjohnso/quickquiz/MainActivity.java
+++ app/src/main/java/com/evanrjohnso/quickquiz/MainActivity.java
@@ -1,13 +1,33 @@
 package com.evanrjohnso.quickquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.start_button)
+    Button mStartLearningButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mStartLearningButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SelectPathActivity.class);
+        startActivity(intent);
+
     }
 }
